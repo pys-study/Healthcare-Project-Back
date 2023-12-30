85d2c603548173cac8d7df6ecf76c280b70ce0b1
asd0236 <tkdgns0236@gmail.com>
2023-12-30 23:07:25 +0900

    Refactor: security 수정

--- hwpSpring/src/main/java/com/hwpBackend/hwpSpring/security/BasicAuthenticationSecurityConfiguration.java
+++ hwpSpring/src/main/java/com/hwpBackend/hwpSpring/security/BasicAuthenticationSecurityConfiguration.java
@@ -5,11 +5,13 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class BasicAuthenticationSecurityConfiguration {
     //스프링 시큐리티 필터 체인
     //모든 요청 인증

6f7022a549c448fbad06f5ed162df437d7a7a4f3
asd0236 <tkdgns0236@gmail.com>
2023-12-30 22:08:55 +0900

    Test: Front react 연결 테스트

--- hwpSpring/src/main/java/com/hwpBackend/hwpSpring/HwpSpringApplication.java
+++ hwpSpring/src/main/java/com/hwpBackend/hwpSpring/HwpSpringApplication.java
@@ -2,12 +2,29 @@ package com.hwpBackend.hwpSpring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@SpringBootApplication
+@SpringBootApplication(exclude= SecurityAutoConfiguration.class)
 public class HwpSpringApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HwpSpringApplication.class, args);
 	}
 
+	// Allow all request only from 3000 to 8000
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedMethods("*")                    // GET, POST, PUT 등 모든 메서드 허용
+						.allowedOrigins("http:localhost:3000"); // ec2 web-server uri 추가할 것
+			}
+		};
+	}
 }

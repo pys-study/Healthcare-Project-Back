78d0852c5568972ceaccd03d501a39f23bb32cd5
asd0236 <tkdgns0236@gmail.com>
2023-12-30 23:02:50 +0900

    Refactor: security 수정

--- hwpSpring/src/main/java/com/hwpBackend/hwpSpring/security/BasicAuthenticationSecurityConfiguration.java
+++ hwpSpring/src/main/java/com/hwpBackend/hwpSpring/security/BasicAuthenticationSecurityConfiguration.java
@@ -1,20 +1,37 @@
 package com.hwpBackend.hwpSpring.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.web.csrf.CsrfFilter;
-import org.springframework.web.filter.CharacterEncodingFilter;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class BasicAuthenticationSecurityConfiguration {
+    //스프링 시큐리티 필터 체인
+    //모든 요청 인증
+    //기본 인증
+    //csrf 비활성화 - 세션 사용하지 않음
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable(); // CSRF 비활성화
-
-        CharacterEncodingFilter filter = new CharacterEncodingFilter();
-        filter.setEncoding("UTF-8");
-        filter.setForceEncoding(true);
-        http.addFilterBefore(filter, CsrfFilter.class);
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        //1. 프리플라이트에 대한 응답이 액세스 제어 체크를 통과하게 함
+        //2. 기본 인증 url
+        return http
+                .authorizeHttpRequests(
+                        auth ->
+                                auth
+                                        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll() //프리플라이트 요청(OPTION 요청) 액세스 허용
+                                        .anyRequest().authenticated()
+                )
+                .httpBasic(Customizer.withDefaults())
+                .sessionManagement(
+                        session -> session.sessionCreationPolicy
+                                (SessionCreationPolicy.STATELESS))
+                .csrf(AbstractHttpConfigurer::disable)
+                .build();
     }
 }

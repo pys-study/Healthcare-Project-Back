44d753577658c29e545cd9846f1273654ce32c4c
asd0236 <tkdgns0236@gmail.com>
2024-01-05 14:13:23 +0900

    Refactor: CORS 수정

--- hwpSpring/src/main/java/com/hwpBackend/hwpSpring/HwpSpringApplication.java
+++ hwpSpring/src/main/java/com/hwpBackend/hwpSpring/HwpSpringApplication.java
@@ -7,12 +7,25 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class HwpSpringApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(HwpSpringApplication.class, args);
     }
 
-
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedMethods("*")                    // GET, POST, PUT 등 모든 메서드 허용
+//                        .allowedOriginPatterns("*") // ec2 web-server uri 추가할 것 // front domain 추가
+                        .allowedOrigins("http://localhost:3000")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
 }

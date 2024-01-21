f592c5999f38eb68cafac8845aa4d2455fb4b236
MCshin00 <sinminc7@gmail.com>
2024-01-21 19:15:37 +0900

    Feat: spring security 관련 설정 변경

--- hwpSpring/src/main/java/com/hwpBackend/hwpSpring/HwpSpringApplication.java
+++ hwpSpring/src/main/java/com/hwpBackend/hwpSpring/HwpSpringApplication.java
@@ -13,20 +13,5 @@ public class HwpSpringApplication {
     public static void main(String[] args) {
         SpringApplication.run(HwpSpringApplication.class, args);
     }
-
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedMethods("*")                    // GET, POST, PUT 등 모든 메서드 허용
-//                        .allowedOriginPatterns("*") // ec2 web-server uri 추가할 것 // front domain 추가
-                        .allowedOrigins("http://localhost:3000")
-                        .allowedOrigins("https://care.healthcare-hwp.com/")
-                        .allowedHeaders("*")
-                        .allowCredentials(true);
-            }
-        };
-    }
+    
 }

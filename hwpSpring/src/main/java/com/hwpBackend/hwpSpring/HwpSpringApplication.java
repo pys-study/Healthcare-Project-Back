0dccaaa376833e560cde270272bb695814c4b9ed
asd0236 <tkdgns0236@gmail.com>
2024-01-22 23:16:12 +0900

    Refactor: origin 수정

--- hwpSpring/src/main/java/com/hwpBackend/hwpSpring/HwpSpringApplication.java
+++ hwpSpring/src/main/java/com/hwpBackend/hwpSpring/HwpSpringApplication.java
@@ -21,10 +21,7 @@ public class HwpSpringApplication {
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
                         .allowedMethods("*")                    // GET, POST, PUT 등 모든 메서드 허용
-//                        .allowedOriginPatterns("*") // ec2 web-server uri 추가할 것 // front domain 추가
-
-                        .allowedOrigins("http://localhost:3000")
-//                        .allowedOrigins("https://care.healthcare-hwp.com")
+                        .allowedOrigins("http://localhost:3000", "https://care.healthcare-hwp.com")
                         .allowedHeaders("*")
                         .allowCredentials(true);
             }

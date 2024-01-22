eb9a6726b6bb54708fd46cf20a699bf0a2b8eb0c
asd0236 <tkdgns0236@gmail.com>
2024-01-22 21:51:24 +0900

    Refactor: 오리진 수정

--- hwpSpring/src/main/java/com/hwpBackend/hwpSpring/HwpSpringApplication.java
+++ hwpSpring/src/main/java/com/hwpBackend/hwpSpring/HwpSpringApplication.java
@@ -22,8 +22,8 @@ public class HwpSpringApplication {
                 registry.addMapping("/**")
                         .allowedMethods("*")                    // GET, POST, PUT 등 모든 메서드 허용
 //                        .allowedOriginPatterns("*") // ec2 web-server uri 추가할 것 // front domain 추가
-//                        .allowedOrigins("http://localhost:3000")
-                        .allowedOrigins("https://care.healthcare-hwp.com/")
+                        .allowedOrigins("http://localhost:3000")
+//                        .allowedOrigins("https://care.healthcare-hwp.com/")
                         .allowedHeaders("*")
                         .allowCredentials(true);
             }
